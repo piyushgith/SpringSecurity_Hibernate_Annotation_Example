ceb35e5151aa6bdca46c0dd68fed061c93284a69
Piyush <piyushprasad2007@gmail.com>
2019-02-03 21:01:22 +0530

    SpringMvc with security

---
+++ src/main/java/Howtorun.java
@@ -0,0 +1,21 @@
+
+
+public class Howtorun {
+
+}
+/*
+run script in DB
+
+mvn clean build -e -DskipTests
+
+deploy it manually on tomcat 8+
+
+http://localhost:8080/SpringSecurityHibernateAnnotationExample/login
+
+ 
+
+
+
+
+
+*/
